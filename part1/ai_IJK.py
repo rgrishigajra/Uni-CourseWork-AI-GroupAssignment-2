905ce02da44b675010f946f815b63f387bdf954b
djcran <djcran@indiana.edu>
2019-10-23 01:22:23 -0400

    initial checkin

---
+++ part1/ai_IJK.py
@@ -0,0 +1,38 @@
+#!/usr/local/bin/python3
+
+"""
+This is where you should write your AI code!
+
+Authors: PLEASE ENTER YOUR NAMES AND USER ID'S HERE
+
+Based on skeleton code by Abhilash Kuhikar, October 2019
+"""
+
+from logic_IJK import Game_IJK
+import random
+
+# Suggests next move to be played by the current player given the current game
+#
+# inputs:
+#     game : Current state of the game 
+#
+# This function should analyze the current state of the game and determine the 
+# best move for the current player. It should then call "yield" on that move.
+
+def next_move(game: Game_IJK)-> None:
+
+    '''board: list of list of strings -> current state of the game
+       current_player: int -> player who will make the next move either ('+') or -'-')
+       deterministic: bool -> either True or False, indicating whether the game is deterministic or not
+    '''
+
+    board = game.getGame()
+    player = game.getCurrentPlayer()
+    deterministic = game.getDeterministic()
+
+    # You'll want to put in your fancy AI code here. For right now this just 
+    # returns a random move.
+
+    yield random.choice(['U', 'D', 'L', 'R', 'S'])
+
+
