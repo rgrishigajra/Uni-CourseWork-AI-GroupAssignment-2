ed1b1153b5ddd6097975cc6f4a5164c3218a94c1
Rishabh gajra <rgrishigajra@gmail.com>
2019-11-09 01:29:22 -0500

    human input viterbi

--- part2/mountain.py
+++ part2/mountain.py
@@ -40,7 +40,8 @@ def draw_edge(image, y_coordinates, color, thickness):
 # main program
 #
 (input_filename, gt_row, gt_col) = sys.argv[1:]
-
+gt_col=int(gt_col)
+gt_row=int(gt_row)
 # load in image 
 input_image = Image.open(input_filename)
 
@@ -58,16 +59,15 @@ imageio.imwrite("output.jpg", draw_edge(input_image, ridge_bayes, (255, 0, 0), 5
 
 ridge_viterbi = argmax(edge_strength, axis=0)
 #print(type(ridge_viterbi))
-
-trans_probab = [0.5,0.2,0.1]
-state_probab = zeros((edge_strength.shape[0], edge_strength.shape[1]))
-max_state = zeros((edge_strength.shape[0],edge_strength.shape[1]))
-#print(max_state.shape)
 total_gradient=zeros(edge_strength.shape[1])
-print(edge_strength.shape[0])
+trans_probab = [0.5,0.25,0.05]
 for col in range(0, edge_strength.shape[1]):
     for row in range(0,edge_strength.shape[0]):
         total_gradient[col]+=edge_strength[row][col]
+state_probab = zeros((edge_strength.shape[0], edge_strength.shape[1]))
+max_state = zeros((edge_strength.shape[0],edge_strength.shape[1]))
+#print(max_state.shape)
+print(edge_strength.shape[0])
 for row in range(0, edge_strength.shape[0]):
     state_probab[row][0] = edge_strength[row][col]/total_gradient[0]
 for col in range(1, edge_strength.shape[1]):
@@ -80,7 +80,7 @@ for col in range(1, edge_strength.shape[1]):
                     maxi = (state_probab[row + j][col - 1]) * (trans_probab[abs(j)])
                     #print(row,col)
                     max_state[row][col] = row + j
-                state_probab[row][col] = (edge_strength[row][col]/10) * (maxi)
+                state_probab[row][col] = (edge_strength[row][col]/100) * (maxi)
     #print(argmax(state_probab), max_state)
 #for row in range(0,140):
 #print(argmax(state_probab[:,250]))
@@ -97,4 +97,29 @@ imageio.imwrite("output.jpg", draw_edge(input_image, ridge_viterbi, (0, 255, 0),
 
 
 ridge_human = [edge_strength.shape[0] / 4] * edge_strength.shape[1]
-# imageio.imwrite("output.jpg", draw_edge(input_image, ridge_human, (0, 0, 255), 5))
+print(type(gt_row),type(gt_col))
+for row in range(0,edge_strength.shape[0]):
+    state_probab[row][gt_col]=0
+state_probab[gt_row][gt_col]=1
+for col in range(gt_col+1, edge_strength.shape[1]):
+    for row in range(gt_row+1, edge_strength.shape[0]):
+        maxi = 0
+        for j in range(-2, 3):
+            # print(i,j)
+            if ((row + j < edge_strength.shape[0]) & (row + j >= 0)):
+                if (maxi< state_probab[row + j][col - 1] * (trans_probab[abs(j)])):
+                    maxi = (state_probab[row + j][col - 1]) * (trans_probab[abs(j)])
+                    #print(row,col)
+                    max_state[row][col] = row + j
+                state_probab[row][col] = (edge_strength[row][col]/100) * (maxi)
+    #print(argmax(state_probab), max_state)
+#for row in range(0,140):
+#print(argmax(state_probab[:,250]))
+#print((state_probab[25,250]))
+#print(state_probab[:,250])
+maxi=argmax(state_probab[:,edge_strength.shape[1]-1])
+for col in range(edge_strength.shape[1]-1,-1,-1):
+    ridge_human[col]=int(maxi)
+    maxi=max_state[int(maxi)][col]
+print(ridge_human)
+imageio.imwrite("output.jpg", draw_edge(input_image, ridge_human, (0, 0, 255), 5))
